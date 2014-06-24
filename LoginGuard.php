8e0244957ebc581f5f652139c372bc5e26cfe7a2
feng3245 <li123b@hotmail.com>
2014-06-24 19:54:54 -0400

    Update LoginGuard.php

--- LoginGuard.php
+++ LoginGuard.php
@@ -1,12 +1,13 @@
 <?php
-include('User.php');
 include('UserRepository.php');
+
 
 class LoginGuard
 {
 	public static function StopForCredentialCheck($userName,$password)
 	{
 		$userRepo = new UserRepository();
+		$user = new User();
 		$user = $userRepo->GetUsersByName($userName);
 		if($user->Password()==$password)
 		{
